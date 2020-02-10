37f85746dbf936fad2738aec70f8a29927d878a1
tnznjamphel <42418634+tnznjamphel@users.noreply.github.com>
2020-02-10 17:05:13 +0530

    Add files via upload

---
+++ singlylinkedlist.cpp
@@ -0,0 +1,62 @@
+//singly linked list
+#include<iostream>
+using namespace std;
+ 
+class Node{
+	public:
+	int data;
+	Node *next;
+	
+};
+class Llist{
+	int length;
+	Node* head;
+	public:
+	Llist(){
+		this->length=0;
+		this->head=NULL;
+	}
+	void insert(int data){
+		Node *node=new Node();
+		Node *temp;
+		node->data=data;
+		if(head==NULL){
+			head=node;
+			node->next=NULL;
+			cout<<"headvalue is changed to "<<head->data;
+		}
+		else{
+			temp=head;
+			while(temp->next!=NULL){
+				temp=temp->next;
+			}
+			temp->next=node;
+		}
+		cout<<"check node values:"<<node->data<<" ";
+    	this->length++;
+    	delete temp;
+	}
+	void traversal(){
+		Node* temp;
+		temp=head;
+		if(head==NULL)
+			cout<<"list is empty";
+		else{
+			while(temp!=NULL){
+				cout<<temp->data<<" ";
+				temp=temp->next;
+		}
+		}
+		delete temp;
+	}
+};
+int main()
+{
+    Llist list1 = Llist();
+    list1.insert(10);
+    list1.insert(20);
+    list1.insert(30);
+    
+    list1.traversal();
+    return 0;
+}
